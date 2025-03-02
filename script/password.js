6dd383263987e98866162907a114841a5a4b9d17
Tugu <200585649@student.georgianc.on.ca>
2025-03-02 17:15:53 -0500

    Update password.js

--- script/password.js
+++ script/password.js
@@ -9,7 +9,7 @@
 
             if (inputPassword === FIXED_PASSWORD) {
                 // Redirect to the protected page
-                window.location.href = "main.html";
+                window.location.href = "main";
             } else {
                 errorMessage.textContent = "Password оо зөв хийгээч";
                 errorMessage.style.display = "block";
